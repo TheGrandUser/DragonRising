e09624e69c1539e8f71885e3c754d710bbe94992
TheGrandUser <thegranduser@outlookc.om>
2014-12-11 23:09:02 -0800

    Missed files

---
+++ DragonRising/Libraries/SimpleEntityLibrary.cs
@@ -0,0 +1,53 @@
+﻿using DraconicEngine;
+using DraconicEngine.GameWorld.Alligences;
+using DraconicEngine.GameWorld.Behaviors;
+using DraconicEngine.GameWorld.EntitySystem;
+using DraconicEngine.GameWorld.EntitySystem.Components;
+using DraconicEngine.Generators;
+using DraconicEngine.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonRising.Libraries
+{
+   class SimpleEntityLibrary : IEntityLibrary
+   {
+      Dictionary<string, EntityTemplate> templates = new Dictionary<string, EntityTemplate>();
+      public Dictionary<string, EntityTemplate> Templates => templates;
+
+      public SimpleEntityLibrary()
+      {
+         var alligence = AlligenceManager.Current.GetByName("Greenskins").Match(
+            Some: a => a,
+            None: () => { throw new Exception("No Greenskins alligence error!"); });
+
+         Add(new EntityTemplate("Orc", new Character(Glyph.OLower, RogueColors.LightGreen),
+            new CombatantComponentTemplate(hp: 10, defense: 0, power: 3),
+            new DecisionComponentTemplate(),
+            new BehaviorComponentTemplate(typeof(BasicMonsterBehavior)),
+            new CreatureComponentTemplate(alligence))
+         { Blocks = true });
+
+
+         Add(new EntityTemplate("Troll", new Character(Glyph.TUpper, RogueColors.LightGreen),
+            new CombatantComponentTemplate(hp: 16, defense: 1, power: 4),
+            new DecisionComponentTemplate(),
+            new BehaviorComponentTemplate(typeof(BasicMonsterBehavior)),
+            new CreatureComponentTemplate(alligence))
+         { Blocks = true });
+      }
+
+      private void Add(EntityTemplate entityTemplate)
+      {
+         this.templates.Add(entityTemplate.Name, entityTemplate);
+      }
+
+      public EntityTemplate Get(string templateName)
+      {
+         return templates[templateName];
+      }
+   }
+}
---
+++ DragonRising/Libraries/SimpleTileLibrary.cs
@@ -0,0 +1,64 @@
+﻿using DraconicEngine.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DraconicEngine;
+
+namespace DragonRising.Libraries
+{
+   class SimpleTileLibrary : ITileLibrary
+   {
+      Dictionary<int, TileType> tileTypes = new Dictionary<int, TileType>();
+
+      public int BasicClearId => 1;
+
+      public int BasicWallId => 2;
+      public int VoidId => 0;
+
+      public TileType GetById(int id)
+      {
+         return tileTypes[id];
+      }
+
+      public SimpleTileLibrary()
+      {
+         Character darkWall = new Character(Glyph.Space, RogueColors.White, new RogueColor(0, 0, 100));
+         Character lightWall = new Character(Glyph.Space, RogueColors.White, new RogueColor(130, 110, 50));
+         Character darkGround = new Character(Glyph.Space, RogueColors.White, new RogueColor(50, 50, 150));
+         Character lightGround = new Character(Glyph.Space, RogueColors.White, new RogueColor(200, 180, 50));
+
+         AddTileType(new TileType()
+         {
+            Id = VoidId,
+            Name = "Void",
+            InView = new Character(Glyph.Space, RogueColors.Black),
+            Explored = new Character(Glyph.Space, RogueColors.Black),
+            BlocksMovement = true,
+            BlocksSight = true
+         });
+         AddTileType(new TileType()
+         {
+            Id = BasicClearId,
+            Name = "Ground",
+            Explored = new Character(Glyph.Space, RogueColors.White, new RogueColor(50, 50, 150)),
+            InView = new Character(Glyph.Space, RogueColors.White, new RogueColor(200, 180, 50)),
+         });
+         AddTileType(new TileType()
+         {
+            Id = BasicWallId,
+            Name = "Stone wall",
+            InView = new Character(Glyph.Space, RogueColors.Black, new RogueColor(130, 110, 50)),
+            Explored = new Character(Glyph.Space, RogueColors.White, new RogueColor(0, 0, 100)),
+            BlocksMovement = true,
+            BlocksSight = true
+         });
+      }
+
+      public void AddTileType(TileType tileType)
+      {
+         this.tileTypes.Add(tileType.Id, tileType);
+      }
+   }
+}
