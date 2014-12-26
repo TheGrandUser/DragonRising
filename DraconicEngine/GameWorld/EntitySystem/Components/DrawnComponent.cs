23caa830b9a9b0cfb4e9d0c135b80b15ebe952a9
TheGrandUser <thegranduser@outlook.com>
2014-12-25 18:30:55 -0800

    Added missing files

---
+++ DraconicEngine/GameWorld/EntitySystem/Components/DrawnComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraconicEngine.GameWorld.EntitySystem.Components
+{
+   public class DrawnComponent : Component
+   {
+      public DrawnComponent()
+      {
+      }
+
+      protected DrawnComponent(DrawnComponent original, bool fresh)
+         : base(original, fresh)
+      {
+         ExploredCharacter = original.ExploredCharacter;
+         SeenCharacter = original.SeenCharacter;
+      }
+
+      public Character SeenCharacter { get; set; }
+      public Character? ExploredCharacter { get; set; }
+
+      protected override Component CloneCore(bool fresh)
+      {
+         return new DrawnComponent(this, fresh);
+      }
+   }
+}
---
+++ DraconicEngine/GameWorld/EntitySystem/Components/LocationComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraconicEngine.GameWorld.EntitySystem.Components
+{
+   public class LocationComponent : Component
+   {
+      public LocationComponent()
+      {
+      }
+
+      protected LocationComponent(LocationComponent original, bool fresh)
+         : base(original, fresh)
+      {
+         Blocks = original.Blocks;
+         Location = fresh ? Loc.Zero : original.Location;
+      }
+
+      public bool Blocks { get; set; }
+      public Loc Location { get; set; }
+
+      protected override Component CloneCore(bool fresh)
+      {
+         return new LocationComponent(this, fresh);
+      }
+   }
+}
