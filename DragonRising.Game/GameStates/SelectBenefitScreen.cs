1fd764eecf7c54e2792d3b9de8bca2c90513f3d5
TheGrandUser <thegranduser@outlookc.om>
2015-01-18 21:53:02 -0800

    Added missing file

---
+++ DragonRising.Game/GameStates/SelectBenefitScreen.cs
@@ -0,0 +1,33 @@
+﻿using DraconicEngine.GameStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
+
+namespace DragonRising.GameStates
+{
+   public class SelectBenefitScreen : GameState
+   {
+      public override GameStateType Type => GameStateType.Screen;
+
+      public override Task Draw()
+      {
+
+         return Task.FromResult(0);
+      }
+
+      public override Task<TickResult> Tick()
+      {
+         return Task.FromResult(TickResult.Finished);
+      }
+   }
+
+   public enum Benefit
+   {
+      Constitution,
+      Strength,
+      Agility,
+   }
+}
