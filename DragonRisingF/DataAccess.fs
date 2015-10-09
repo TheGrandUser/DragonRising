fbf91540c68c1428c3b9897463814afd3d1d1bb4
TheGrandUser <thegranduser@outlook.com>
2015-10-08 18:02:16 -0700

    added missing files

---
+++ DragonRisingF/DataAccess.fs
@@ -0,0 +1,15 @@
+﻿module DataAccess
+
+open DragonRisingF.DomainTypes
+
+type ISaveManager =
+   abstract member SaveGame: string -> World -> Async<unit>
+   abstract member LoadGame: string -> Async<World>
+   abstract member SavedGames: seq<string>
+   abstract member LastSavedGame: string option
+
+
+let saveGame (sm: ISaveManager) = sm.SaveGame
+let loadGame (sm: ISaveManager) = sm.LoadGame
+let getSavedGames (sm: ISaveManager) = sm.SavedGames
+let lastSavedGame (sm: ISaveManager) = sm.LastSavedGame
